77371c19b301db488a62f1ddbdcaa965786ca924
Ian Monroe <ian@ianmonroe.com>
2024-03-07 12:41:06 -0700

    rebuilt theme

--- docroot/modules/custom/stanford_profile_helper/stanford_profile_helper.post_update.php
+++ docroot/modules/custom/stanford_profile_helper/stanford_profile_helper.post_update.php
@@ -5,6 +5,20 @@
  * stanford_profile_helper.post_update.php
  */
 
+ /**
+ * Create new ultimate cron job.
+ */
+function stanford_profile_helper_post_update_create_cron(&$sandbox) {
+  \Drupal::entityTypeManager()
+    ->getStorage('ultimate_cron_job')
+    ->create([
+      'id' => 'stanford_profile_helper_cron',
+      'label' => 'Stanford Profile Helper Cron',
+      'module' => 'stanford_profile_helper',
+      'callback' => 'stanford_profile_helper_cron',
+    ])->save();
+}
+
 /**
  * Implements hook_removed_post_updates().
  */
@@ -18,6 +32,5 @@ function stanford_profile_helper_removed_post_updates() {
     'stanford_profile_helper_post_update_8103' => '9.4.3',
     'stanford_profile_helper_post_update_9000' => '9.4.3',
     'stanford_profile_helper_post_update_9001' => '9.4.3',
-    'stanford_profile_helper_post_update_create_cron' => '9.4.3',
   ];
 }
