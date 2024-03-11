bdfc573ade812541a12352de6844a120ab2c7cad
Ian Monroe <ian@ianmonroe.com>
2024-03-11 11:42:09 -0600

    unlocked stanford_profile_helper

--- docroot/modules/custom/stanford_profile_helper/stanford_profile_helper.post_update.php
+++ docroot/modules/custom/stanford_profile_helper/stanford_profile_helper.post_update.php
@@ -18,5 +18,6 @@ function stanford_profile_helper_removed_post_updates() {
     'stanford_profile_helper_post_update_8103' => '9.4.3',
     'stanford_profile_helper_post_update_9000' => '9.4.3',
     'stanford_profile_helper_post_update_9001' => '9.4.3',
+    // 'stanford_profile_helper_post_update_create_cron' => '9.4.3',
   ];
 }
